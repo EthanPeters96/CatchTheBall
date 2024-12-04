53eec8b2ee0306960f259dd0395828e7952d98ec
Ethan Peters <et8lg2pz@students.codeinstitute.net>
2024-12-04 21:08:28 +0000

    refactor
    refactor testing file

--- assets/js/script.test.js
+++ assets/js/script.test.js
@@ -1,22 +1,36 @@
 jest.useFakeTimers();
 
-jest.mock("./script", () => {
-    const createBall = jest.fn();
-    const startGame = jest.fn(() => {
-        setInterval(() => {
-            createBall();
+jest.mock("./script", () => ({
+    createBall: jest.fn(),
+    startGame: jest.fn(function mockStartGame() {
+        return setInterval(() => {
+            this.createBall();
         }, 1000);
-    });
-
-    return { startGame, createBall };
-});
+    }),
+}));
 
 const script = require("./script");
 
 describe("Game Functions", () => {
-    test("should create a ball every 1 second", () => {
-        script.startGame();
-        jest.advanceTimersByTime(1000);
-        expect(script.createBall).toHaveBeenCalledTimes(1);
+    beforeEach(() => {
+        jest.clearAllMocks();
+        jest.clearAllTimers();
+    });
+
+    describe("startGame", () => {
+        test("should create a ball every 1 second", () => {
+            script.startGame();
+            jest.advanceTimersByTime(1000);
+            expect(script.createBall).toHaveBeenCalledTimes(1);
+
+            jest.advanceTimersByTime(1000);
+            expect(script.createBall).toHaveBeenCalledTimes(2);
+        });
+
+        test("should continue creating balls over time", () => {
+            script.startGame();
+            jest.advanceTimersByTime(5000);
+            expect(script.createBall).toHaveBeenCalledTimes(5);
+        });
     });
 });
